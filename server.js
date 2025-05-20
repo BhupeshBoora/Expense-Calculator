9f833f505708c84ccf9d7d62661bc657bae0e6fe
Boora <bhupeshboora2nd@gmail.com>
2025-05-20 15:27:02 +0530

    Addition of Expense & Full Report at /report

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import pg, { Client } from "pg";
 
 const myApp = express();
 const myPort = 3000;
+const today = new Date().toISOString().split('T')[0];
 const db = new pg.Client
 ({
     user: "postgres",
@@ -17,15 +18,15 @@ myApp.use(bodyParser.urlencoded({extended:true}));
 myApp.use(express.static("public"));
 db.connect();
 
-
-myApp.listen(myPort, () =>
-{
-    console.log(`Server is running on Port: ${myPort}`);
-});
-
 myApp.get("/", (req, res) =>
 {
-    res.render("index.ejs");
+    res.render("index.ejs",
+        {
+            today: 200,
+            thisWeek: 500,
+            thisMonth: 1000
+        }
+    );
 });
 
 myApp.post("/add", async (req, res) =>
@@ -34,6 +35,27 @@ myApp.post("/add", async (req, res) =>
     var category = req.body.category;
     var date = req.body.date;
 
-    var result = await db.query("INSERT INTO spent (time, money_spent, category) VALUES($1, $2, $3)",
+    await db.query("INSERT INTO spent (time, money_spent, category) VALUES($1, $2, $3)",
         [date, amount, category]);
+
+    res.redirect("/");
+});
+
+myApp.get("/report", async (req, res) =>
+{
+    const data = await generateReport();
+    res.send(data);
+});
+
+async function generateReport()
+{
+    const result = await db.query(
+        "SELECT TO_CHAR(time, 'DD-MM-YYYY') AS date, money_spent, category FROM spent ORDER BY date ASC");
+    
+    return result.rows;
+}
+
+myApp.listen(myPort, () =>
+{
+    console.log(`Server is running on Port: ${myPort}`);
 });
